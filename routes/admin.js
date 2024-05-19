724dda359cbd0b3983c449cc57d5d46c2f785956
Kevin BUSTY <kevinbusty@gmail.com>
2024-05-19 20:36:30 +0400

    EJS

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,29 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 
 const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const products = [];
+
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
